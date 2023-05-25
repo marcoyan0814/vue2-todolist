6f637108a753a3a055d55692f0e8b1068ecb18ae
marcoyan0814 <marcoyan0814@gmail.com>
2023-05-25 20:02:47 +0800

    feat: 加上animate.css套件及在增/刪時實現動畫效果

--- src/components/TodoLists.vue
+++ src/components/TodoLists.vue
@@ -4,16 +4,26 @@
             <span class="text-primary">目前待辦清單</span>
         </h4>
         <ul class="list-group mb-3">
-            <TodoItem 
-                v-for="todo in todos" 
-                :key="todo.id" 
-                :todo="todo" 
-            />
+            <TransitionGroup 
+                name="todo" 
+                class="animate__animated animate__bounce"
+                apper
+                enter-active-class="animate__animated animate__fadeInRight"
+                leave-active-class="animate__animated animate__fadeOutLeft"               
+            >
+                <TodoItem 
+                    v-for="todo in todos" 
+                    :key="todo.id" 
+                    :todo="todo" 
+                />
+            </TransitionGroup>
         </ul>
     </div>
 </template>
 
 <script>
+    //引入 animate.css
+    import 'animate.css'
     //引入 components
     import TodoItem from './TodoItem'
     export default {
