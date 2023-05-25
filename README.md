2e621573f93f45156dc95d9379d166cb7a0132dd
marcoyan0814 <marcoyan0814@gmail.com>
2023-05-25 08:51:28 +0800

    docs: 版面及程式初始化設定完畢

--- README.md
+++ README.md
@@ -1,24 +1,58 @@
-# vue2-todolist
+# Vue2 + todolist 待辦事項 練習題
+
+### 建立專案
+
+```
+vue create vue2-todolist
+```
+
+### 安裝套件
+
+```
+vue create vue2-totolist
+// 隨機產生uuid輕量版
+npm install nanoid
+// 安裝bootstrap 相關套件
+npm install @popperjs/core
+npm install bootstrap
+```
+
+### 設定 bootstrap 相關
+
+```
+// 修改 main.js ，在 new Vue 前插入
+import "bootstrap/dist/css/bootstrap.min.css"
+
+// 在 new Vue 後插入
+import "bootstrap/dist/js/bootstrap.js"
+```
+
+### 啟動開發測試
+
+```
+npm run serve
+```
 
 ## Project setup
+
 ```
 npm install
 ```
 
 ### Compiles and hot-reloads for development
+
 ```
 npm run serve
 ```
 
 ### Compiles and minifies for production
+
 ```
 npm run build
 ```
 
 ### Lints and fixes files
+
 ```
 npm run lint
 ```
-
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,66 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <div class="mt-5">
+      <div class="col-12">
+        <input type="text" class="form-control" placeholder="請輸入待辦項目，輸入完畢請按Enter" required="true">
+      </div>
+      <h4 class="m-3">
+        <span class="text-primary">目前待辦清單</span>
+      </h4>
+      <ul class="list-group mb-3">
+        <li class="p-3 list-group-item d-flex justify-content-between lh-sm">
+          <div class="form-check pt-2 pb-2">
+            <input type="checkbox" class="form-check-input" id="same-address">
+            <label class="form-check-label" for="same-address">Shipping address is the same as my billing
+              address</label>
+          </div>
+          <div class="float-right"><button class="btn btn-danger hide">刪除項目</button></div>
+        </li>
+        <li class="p-3 list-group-item d-flex justify-content-between lh-sm">
+          <div class="form-check pt-2 pb-2">
+            <input type="checkbox" class="form-check-input" id="same-address">
+            <label class="form-check-label" for="same-address">Shipping address is the same as my billing
+              address</label>
+          </div>
+          <div class="float-right"><button class="btn btn-danger hide">刪除項目</button></div>
+        </li>
+        <li class="p-3 list-group-item d-flex justify-content-between lh-sm">
+          <div class="form-check pt-2 pb-2">
+            <input type="checkbox" class="form-check-input" id="same-address">
+            <label class="form-check-label" for="same-address">Shipping address is the same as my billing
+              address</label>
+          </div>
+          <div class="float-right"><button class="btn btn-danger hide">刪除項目</button></div>
+        </li>
+      </ul>
+
+      <div class="p-3 list-group-item d-flex justify-content-between lh-sm">
+        <div class="float-left form-check my-auto">
+          <label>
+            <input type="checkbox" class="form-check-input"> 全部完成/全部未完成
+          </label>
+        </div>
+        <div class="float-right">
+          <span class="mx-3"> 已完成 0 筆 / 總共有 0 筆</span>
+          <button type="button" class="btn btn-danger">刪除已完成項目</button>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  export default {
+    name: "App",
+  };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  button.hide {
+    display: none;
+  }
+
+  li:hover .btn {
+    display: block;
+  }
 </style>
